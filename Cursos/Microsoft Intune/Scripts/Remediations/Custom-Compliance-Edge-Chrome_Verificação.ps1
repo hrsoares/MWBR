8f786a94d585bca55d003b599cdd4717014422ca
Henver Soares <46744916+hrsoares@users.noreply.github.com>
2024-08-13 00:00:39 -0300

    Add files via upload

---
+++ Cursos/Microsoft Intune/Scripts/Remediations/Custom-Compliance-Edge-Chrome_Verificação.ps1
@@ -0,0 +1,24 @@
+﻿# Verificar versão instalada do Edge
+$edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+if (Test-Path $edgePath) {
+    $edgeVersion = (Get-Item $edgePath).VersionInfo.ProductVersion
+} else {
+    $edgeVersion = "Não instalado"
+}
+
+# Verificar versão instalada do Chrome
+$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+if (Test-Path $chromePath) {
+    $chromeVersion = (Get-Item $chromePath).VersionInfo.ProductVersion
+} else {
+    $chromeVersion = "Não instalado"
+}
+
+# Output do status de descoberta
+$discoveryStatus = @{
+    EdgeInstalledVersion = $edgeVersion
+    ChromeInstalledVersion = $chromeVersion
+}
+
+# Retornar status como JSON
+$discoveryStatus | ConvertTo-Json -Compress
